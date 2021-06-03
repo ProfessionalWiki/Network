b408ff0d08d97187b87209de0a23fd70e34bde7f
Cindy Cicalese <cindom@gmail.com>
2021-06-02 22:04:13 -0400

    Fix excluded namespaces for parser function

--- src/NetworkFunction/NetworkConfig.php
+++ src/NetworkFunction/NetworkConfig.php
@@ -33,7 +33,7 @@ class NetworkConfig {
 	public function __construct() {
 		$this->options = $GLOBALS['wgPageNetworkOptions'];
 		$this->excludeTalkPages = (bool)$GLOBALS['wgPageNetworkExcludeTalkPages'];
-		$this->excludedNamespaces = $GLOBALS['wgPageNetworkExcludedNamespaces'];
+		$this->excludedNamespaces = array_map( 'strval', $GLOBALS['wgPageNetworkExcludedNamespaces'] );
 		$this->enableDisplayTitle = (bool)$GLOBALS['wgPageNetworkEnableDisplayTitle'];
 		$this->labelMaxLength = (int)$GLOBALS['wgPageNetworkLabelMaxLength'];
 	}
