17ede5327621bc15abc636bce672740db4b41c8a
SomeRandomDeveloper <thisisnotmyname275@gmail.com>
2025-07-22 02:30:33 +0200

    Add imports for namespaced Html and Title classes
    Fixes compatibility with MW 1.44.
    Raise MW requirement to 1.43.
    Run CI on 1.44.
    Bump version to 4.0.0.

--- tests/php/NetworkFunctionIntegrationTest.php
+++ tests/php/NetworkFunctionIntegrationTest.php
@@ -5,6 +5,7 @@ declare( strict_types = 1 );
 namespace MediaWiki\Extension\Network\Tests;
 
 use MediaWiki\MediaWikiServices;
+use MediaWiki\Title\Title;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -16,7 +17,7 @@ class NetworkFunctionIntegrationTest extends TestCase {
 
 	private function parse( string $textToParse ): string {
 		return MediaWikiServices::getInstance()->getParser()
-			->parse( $textToParse, \Title::newFromText( self::PAGE_TITLE ), new \ParserOptions( \User::newSystemUser( 'TestUser' ) ) )->getText();
+			->parse( $textToParse, Title::newFromText( self::PAGE_TITLE ), new \ParserOptions( \User::newSystemUser( 'TestUser' ) ) )->getText();
 	}
 
 	public function testWhenThereAreNoParameters_contextPageIsUsed() {
